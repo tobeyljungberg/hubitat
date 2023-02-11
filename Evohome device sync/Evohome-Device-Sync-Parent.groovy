91552dee88a571704163ba5c946c2802d3b8b367
Tobey Ljungberg <tobeyljungberg@icloud.com>
2023-02-11 21:13:34 +0000

    Initial

---
+++ Evohome device sync/Evohome-Device-Sync-Parent.groovy
@@ -0,0 +1,64 @@
+
+definition(
+    name:"Evohome Device Sync",
+    namespace:"tljungberg",
+    author:"Tobey Ljungberg",
+    description: "Parent app for Evohome Device Sync",
+    category: "Convenience",
+    iconUrl: "",
+    iconX2Url: "",
+    iconX3Url: "",
+)
+
+
+preferences {
+    page name: "mainPage", title: "", install: true, uninstall: true
+}
+
+def installed() {
+    log.debug "Installed using settings: ${settings}"
+    initialize()
+}
+
+def initialize() {
+    setVersion()
+    log.info "Evohome Device Sync Initialized"
+}
+
+
+def mainPage() {
+    dynamicPage(name: "mainPage") {
+        installCheck()
+
+        if (state.appInstalled == 'COMPLETE') {
+            display()
+
+            section ("") {
+                app(name: "EvoHomeDeviceSync", appName: "Evohome Device Sync", namespace: "tljungberg", title: "Add a new sync app", multiple: true)
+            }
+
+        }
+    }
+}
+
+
+def display() {
+    section{paragraph "Version: $state.version"}
+}
+
+
+def installCheck() {
+    state.appInstalled = app.getInstallationState()
+    if(state.appInstalled != 'COMPLETE'){
+        section{paragraph "Please hit 'Done' to install '${app.label}' parent app "}
+    } else {
+        log.info "Parent Installed OK"
+    }
+}
+
+
+def setVersion() {
+		state.version = "1.0"
+    state.internalName = "EvoHomeDeviceSyncDevices"
+    state.externalName = "Evhome Device Sync Devices"
+}
